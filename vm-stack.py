8c56aa0d9a5856aacf21ffd8a3e19eaa99ab6ac3
Daniel Burov <burovdaniel@gmail.com>
2022-01-04 21:42:57 +0000

    added arg1 + 2 for the commands

--- vm-stack.py
+++ vm-stack.py
@@ -19,10 +19,18 @@ class Parser:
         command_type_dict = {'pop':'C_POP','push':'C_PUSH'}
         command_type_dict.update(dict.fromkeys(['eq','sub','neg','add','gt','lt','and','or','not'],'C_ARITHMATIC'))
 
-        self.command_type = command_type_dict[line.split()[0]]
+        self.command_type = command_type_dict[line.split()[0]] #gives the cammand type
+
+        #gives the argmunets of the command
+        if self.command_type is 'C_ARITHMATIC':
+            self.arg1 = str(line.split()[0])
+            self.arg2 = 'None'
+        else:
+            self.arg1 = str(line.split()[1])
+            self.arg2 = int(line.split()[2])
 
 if __name__ == '__main__':
     code = Constructer()
     for line in code:
         line = Parser(line)
-        print(line.command_type)
+        print(line.arg2)

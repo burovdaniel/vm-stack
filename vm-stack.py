76726405b87e4ed13a4ff504eb92b534fc431726
Daniel Burov <burovdaniel@gmail.com>
2022-01-04 18:02:46 +0000

    added command type attrubite for lines

--- vm-stack.py
+++ vm-stack.py
@@ -6,13 +6,23 @@ def Constructer():#reads the vm file
         code = VM_code.readlines()
 
     code = [line.replace('\n','') for line in code] #remove newline
-
-    code = [line for line in code if not line.startswith('//')]
-
+    code = [line for line in code if not line.startswith('//')]#removes comments
+    code = [line for line in code if not line is  '']
     return code
 
 
+class Parser:
+
+    def __init__(self,line):
+
+        #dict for the command type
+        command_type_dict = {'pop':'C_POP','push':'C_PUSH'}
+        command_type_dict.update(dict.fromkeys(['eq','sub','neg','add','gt','lt','and','or','not'],'C_ARITHMATIC'))
+
+        self.command_type = command_type_dict[line.split()[0]]
 
 if __name__ == '__main__':
     code = Constructer()
-    print(code)
+    for line in code:
+        line = Parser(line)
+        print(line.command_type)
